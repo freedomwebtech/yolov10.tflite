559fcfb17c73d9bbf12667d3d09aee772cb9be8a
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2024-08-22 19:02:42 +0530

    Update pmain1.py

--- pmain1.py
+++ pmain1.py
@@ -2,9 +2,9 @@ import cv2
 from ultralytics import YOLO
 import pandas as pd
 import cvzone
-from tracker import*
 
-model = YOLO("yolov10n.pt")  
+
+model = YOLO("")  
  
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
@@ -46,21 +46,9 @@ while True:
         y2 = int(row[3])
         d = int(row[5])
         c = class_list[d]
-        list.append([x1,y1,x2,y2])
-    bbox_idx=tracker.update(list)
-    for bbox in bbox_idx:
-        x3,y3,x4,y4,id=bbox
-        cx=int(x3+x4)//2
-        cy=int(y3+y4)//2
-        if cy1<(cy+offset) and cy1>(cy-offset):
-           up[id]=(cx,cy) 
-            
-           cv2.circle(frame,(cx,cy),4,(255,0,0),-1)
-           cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
-           cv2.rectangle(frame,(x3,y3),(x4,y4),(255,255,0),2)
-    print(up)          
-    cv2.line(frame,(203,287),(1019,287),(255,0,255),2)
-    cv2.line(frame,(165,305),(1019,305),(255,255,255),2)
+        cvzone.putTextRect(frame,f'{id}',(x1,y1),1,1)
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(255,255,0),2)
+   
     cv2.imshow("RGB", frame)
     # Break the loop if 'q' is pressed
     if cv2.waitKey(0) & 0xFF == ord('q'):
